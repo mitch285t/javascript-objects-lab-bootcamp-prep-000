fbfd91f8877dd2736a7955748be40f914a435cef
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-17 20:41:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function updateObjectWithKeyAndValue(recipes, key, value){
       return Object.assign({}, recipes, { ['prop2']: 2 })
 }
 function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
-  
+  recipes['prop3'] = 3
 }
